505043f788d03a2587d0432774c93d5abe0d27bc
Paul Meinz <paulmeinz@gmail.com>
2023-07-25 13:28:49 -0700

    FIRST COMMIT; ADDED set_dsn()

---
+++ R/set_dsn.R
@@ -0,0 +1,10 @@
+set_dsn <- function(irdb3 = NULL,
+                    dopsoftrdm = NULL,
+                    db01 = NULL,
+                    db02 = NULL) {
+
+  options(irdb3 = irdb3)
+  options(dopsoftrdm = dopsoftrdm)
+  options(db01 = db01)
+  options(db02 = db02)
+}
