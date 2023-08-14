870fc631e4d0b9dafd5ad78aba2ac012ba93fe69
Paul Meinz <paulmeinz@gmail.com>
2023-08-14 15:41:33 -0700

    Fixed an issue with set_dsn()

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,12 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage(crayon::green('Welcome to ezHawkSQL!'))
   dsn_values <- eval(parse(text = Sys.getenv('ezHawkDSN')))
-  if(dsn_values == "") {suppressWarnings(set_dsn(dsn = dsn_values))}
+  if(!is.null(dsn_values)) {suppressWarnings(set_dsn(dsn = dsn_values))}
   dsn_check <- is.null(options()$ez.dsn)
-  if (dsn_check) {e
+  if (dsn_check) {
     packageStartupMessage(crayon::red("\U2717 - Detecting no DSNs!"))
-    packageStartupMessage(crayon::red("Make sure to set your DSN using set_dsn()"))
+    packageStartupMessage(crayon::red(strwrap("Make sure to set your DSN
+                                              using set_dsn() or save_dsn()")))
   } else {
     dsn_default <- options()$ez.dsn.default
     packageStartupMessage(crayon::green("\nYour current default DSN is:"))
